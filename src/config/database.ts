4ae99518f281400a7688c900b2721d6d0b24a7c3
EmilianoTL <atorresl1900@gmail.com>
2025-01-08 13:50:41 -0600

    Update ssh database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,15 +10,15 @@ const sslCertPath = path.join(process.cwd(), 'certificates', 'us-east-2-bundle.p
 
 // Pool de conexiones
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    /*ssl: {
-      rejectUnauthorized: true, // Rechaza conexiones no autorizadas
-      ca: fs.readFileSync(sslCertPath).toString(), // Lee el archivo .pem
-    },*/
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true, // Rechaza conexiones no autorizadas
+    ca: fs.readFileSync(sslCertPath).toString(), // Lee el archivo .pem
+  },
 });
 
 export default pool;
