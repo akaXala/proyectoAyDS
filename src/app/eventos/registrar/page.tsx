15146c8d0f0d33e86b67a39e9eec950edb874be6
akaXala <ajimenezx1900@alumno.ipn.mx>
2025-01-12 20:49:55 -0600

    Fix DB

--- src/app/eventos/registrar/page.tsx
+++ src/app/eventos/registrar/page.tsx
@@ -27,7 +27,7 @@ const EventoClub = [
   { nombre_evento: "Carrera 5k", tipo_evento: "Cinta", capacidad: 100, modalidad: "Individual", imagen: "/Carrera5kCinta.webp", label: "Carrera 5k en Cinta" },
   { nombre_evento: "Carrera 5k", tipo_evento: "Exterior", capacidad: 1000, modalidad: "Individual", imagen: "/Carrera5kExterior.webp", label: "Carrera 5k en Exterior" },
   { nombre_evento: "Corre en cinta", tipo_evento: "Carrera en cinta", capacidad: 20, modalidad: "Individual", imagen: "/CarreraEnCinta.webp", label: "Corre en cinta" },
-  { nombre_evento: "Triatlón in-doors", tipo_evento: "Natación, ciclismo y carrera en cinta", modalidad: "Individual", capacidad: 40, imagen: "https://example.com/imagenes/triatlon-in-doors.jpg", label: "Triatlón in-doors" },
+  { nombre_evento: "Triatlón in-doors", tipo_evento: "Natación, ciclismo y carrera en cinta", modalidad: "Individual", capacidad: 40, imagen: "/TriatlonInDoors.webp", label: "Triatlón in-doors" },
   { nombre_evento: "Maratón de nado con aletas", tipo_evento: "Natación", capacidad: 25, modalidad: "Individual", imagen: "https://example.com/imagenes/maraton-nado-aletas.jpg", label: "Maratón de nado con aletas" },
   { nombre_evento: "Maratón de nado con snorkel", tipo_evento: "Natación", capacidad: 25, modalidad: "Individual", imagen: "https://example.com/imagenes/maraton-nado-snorkel.jpg", label: "Maratón de nado con snorkel" },
   { nombre_evento: "Macro-zumba", tipo_evento: "Zumba", capacidad: 300, modalidad: "Individual", imagen: "https://example.com/imagenes/macro-zumba.jpg", label: "Macro-zumba" },
